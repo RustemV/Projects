e9a724881870a72fe3e9aa3934186d252800f43f
RustemV <rustemv@gmail.com>
2021-10-02 19:32:22 +0300

    save @home 02/10/21 19-32

--- Minin/ChtoTakoeRESTAPI_SozdaemAPInaExpress/RESTAPI/servapp.js
+++ Minin/ChtoTakoeRESTAPI_SozdaemAPInaExpress/RESTAPI/servapp.js
@@ -2,5 +2,4 @@ const express = require(express);
 // создание объекта приложения через функцию 
 const app = express();
 
-app.listen(3000, () => console.log(`Server has been started on port 3000...`););
-
+//app.listen(3000, () => console.log(`Server has been started on port 3000...`));
