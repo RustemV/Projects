27945a011868793577689eaa84d8040ce7b76583
rustemv <rustemv@gmail.com>
2021-10-21 19:46:50 +0300

    saved @work 21/10/21 19-46

--- Udemy/01_PolniyCursPoJavaScriptPlusReact/Project_2.28_Udemy/js/script.js
+++ Udemy/01_PolniyCursPoJavaScriptPlusReact/Project_2.28_Udemy/js/script.js
@@ -6,5 +6,19 @@ const box = document.getElementById('box'),
     hearts = document.querySelectorAll('.heart'),
     oneHeart = document.querySelector('.heart');
 
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = `background-color: blue; width: ${num}px`;
+
+btns[1].style.borderRadius = '100%';
+
+circles[0].style.backgroundColor = 'red';
+
+hearts.forEach( item => {
+    item.style.backgroundColor = 'blue'
+});
+
+const div = document.createElement('div'); //в скобках название тега
+
+const text = document.createTextNode('просто текст');
