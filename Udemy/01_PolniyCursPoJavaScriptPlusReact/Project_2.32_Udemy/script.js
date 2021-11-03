f5bd608ad4e00db04ffb50198490dcff24de0006
RustemV <rustemv@gmail.com>
2021-11-03 21:55:31 +0300

    saved @home 03/11/21 21-55

---
+++ Udemy/01_PolniyCursPoJavaScriptPlusReact/Project_2.32_Udemy/script.js
@@ -0,0 +1,18 @@
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').nextSibling);
+// console.log(document.querySelector('#current').previousSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
--- UlbiTV/node_postgres/db.js
+++ UlbiTV/node_postgres/db.js
@@ -1,12 +1,12 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     password: '23',
     host: 'localhost',
     port: 5434,
     database: 'node_postgres'
-})
+});
 
 
 
-module.exports = pool
+module.exports = pool;
